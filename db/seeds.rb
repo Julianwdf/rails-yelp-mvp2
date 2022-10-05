6f606567d2912ed15ee607bda93d64a2e469bb89
Julian Wong <julian.df.wong@gmail.com>
2022-10-05 16:24:50 +0800

    my app is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: 'italian'
+  )
+  restaurant.save!
+end
